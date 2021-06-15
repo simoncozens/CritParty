fa506ab66f62a9a156f9bd5922d8dd85a57398d8
schriftgestalt <georg.seifert@mac.com>
2021-06-15 14:22:19 +0200

    stick with the deprecated method

--- CritParty/CritParty/SignalingClient.m
+++ CritParty/CritParty/SignalingClient.m
@@ -49,7 +49,7 @@ NSString *url = @"ws://critparty.corvelsoftware.co.uk:9000/";
 		[[NSString alloc] initWithData:messageJSONObject
 		 encoding:NSUTF8StringEncoding];
 	SCLog(@"Sending to ws: %@", messageString);
-	[_socket sendString:messageString error:nil];
+	[_socket send:messageString];
 }
 
 // MARK: - SocketRocket delegate methods
